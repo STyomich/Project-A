60834e48422c5327983adf86baac47b78c19d960
Artyom <artem.stepanenko.kit@gmail.com>
2025-01-29 20:01:23 +0200

    Setting up docker compose for dotnet application and sqlserver

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [ApiController]
-    [AllowAnonymous]
+
     [Route("api/[controller]")]
     public class BaseApiController :ControllerBase
     {
--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
+builder.WebHost.UseUrls("http://*:5000"); // Set port 5000
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
